5b9814f96775988a8e78af332d7d6ff1d6696c74
alfimonth <alvy1610@gmail.com>
2023-03-31 19:52:05 +0700

    Membuat database

---
+++ lib/model/database.dart
@@ -0,0 +1,64 @@
+List<Map> pegawai = [
+  {
+    'id': 1,
+    'nip': '545664',
+    'nama': 'Eko Purnomo',
+    'tanggalLahir': '15 Oktober 1987',
+    'nomorTelepon': '081887674598',
+    'email': 'eko@gmail.com',
+    'password': '123',
+  },
+  {
+    'id': 2,
+    'nip': '6576455',
+    'nama': 'Dwi Febrianto',
+    'tanggalLahir': '9 Juli 1977',
+    'nomorTelepon': '085665765490',
+    'email': 'dwi@gmail.com',
+    'password': '123',
+  },
+  {
+    'id': 3,
+    'nip': '668687',
+    'nama': 'Tri Cahyono',
+    'tanggalLahir': '9 Agustus 1979',
+    'nomorTelepon': '085698798906',
+    'email': 'tri@gmail.com',
+    'password': '123',
+  },
+];
+
+List<Map> pasien = [
+  {
+    'id': 1,
+    'noRm': '11873',
+    'nama': 'Rendi Santoso',
+    'tanggalLahir': '10 Oktober 1997',
+    'nomorTelepon': '08188767459',
+    'alamat': 'Sukoharjo',
+  },
+  {
+    'id': 2,
+    'noRm': '7678',
+    'nama': 'Suharyani',
+    'tanggalLahir': '20 Juni 2000',
+    'nomorTelepon': '0876766453',
+    'alamat': 'Klaten',
+  },
+  {
+    'id': 3,
+    'noRm': '67678',
+    'nama': 'Adi Nugroho',
+    'tanggalLahir': '11 Februari 1999',
+    'nomorTelepon': '08987666400',
+    'alamat': 'Pacitan',
+  },
+  {
+    'id': 4,
+    'noRm': '67678',
+    'nama': 'Wahyu Setianto',
+    'tanggalLahir': '5 Januari 1998',
+    'nomorTelepon': '08987666454',
+    'alamat': 'Boyolali',
+  },
+];
