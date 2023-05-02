21fde7be1a816c3018749760909f136c50f4586c
alfimonth <alvy1610@gmail.com>
2023-05-02 12:59:15 +0700

    Membuat form ubah poli

--- lib/ui/poli_detail.dart
+++ lib/ui/poli_detail.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'poli_update_form.dart';
 import '../model/poli.dart';
 
 class PoliDetail extends StatefulWidget {
@@ -15,27 +16,42 @@ class _PoliDetailState extends State<PoliDetail> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("Detail Poli")),
-      body: Column(children: [
-        SizedBox(height: 20),
-        Text(
-          "Nama Poli : ${widget.poli.namaPoli}",
-          style: TextStyle(fontSize: 20),
-        ),
-        SizedBox(height: 20),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            ElevatedButton(
-                onPressed: () {},
-                style: ElevatedButton.styleFrom(backgroundColor: Colors.green),
-                child: const Text("Ubah")),
-            ElevatedButton(
-                onPressed: () {},
-                style: ElevatedButton.styleFrom(backgroundColor: Colors.red),
-                child: const Text("Hapus")),
-          ],
-        )
-      ]),
+      body: Column(
+        children: [
+          SizedBox(height: 20),
+          Text(
+            "Nama Poli : ${widget.poli.namaPoli}",
+            style: TextStyle(fontSize: 20),
+          ),
+          SizedBox(height: 20),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              _tombolUbah(),
+              _tombolHapus(),
+            ],
+          )
+        ],
+      ),
     );
+  }
+
+  _tombolUbah() {
+    return ElevatedButton(
+        onPressed: () {
+          Navigator.push(
+              context,
+              MaterialPageRoute(
+                  builder: (context) => PoliUpdateForm(poli: widget.poli)));
+        },
+        style: ElevatedButton.styleFrom(backgroundColor: Colors.green),
+        child: const Text("Ubah"));
+  }
+
+  _tombolHapus() {
+    return ElevatedButton(
+        onPressed: () {},
+        style: ElevatedButton.styleFrom(backgroundColor: Colors.red),
+        child: const Text("Hapus"));
   }
 }
---
+++ lib/ui/poli_update_form.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import '../model/poli.dart';
+import '../ui/poli_detail.dart';
+
+class PoliUpdateForm extends StatefulWidget {
+  final Poli poli;
+
+  const PoliUpdateForm({Key? key, required this.poli}) : super(key: key);
+  _PoliUpdateFormState createState() => _PoliUpdateFormState();
+}
+
+class _PoliUpdateFormState extends State<PoliUpdateForm> {
+  final _formKey = GlobalKey<FormState>();
+  final _namaPoliCtrl = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    setState(() {
+      _namaPoliCtrl.text = widget.poli.namaPoli;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Ubah Poli")),
+      body: SingleChildScrollView(
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: [_fieldNamaPoli(), SizedBox(height: 20), _tombolSimpan()],
+          ),
+        ),
+      ),
+    );
+  }
+
+  _fieldNamaPoli() {
+    return TextField(
+      decoration: const InputDecoration(labelText: "Nama Poli"),
+      controller: _namaPoliCtrl,
+    );
+  }
+
+  _tombolSimpan() {
+    return ElevatedButton(
+        onPressed: () {
+          Poli poli = new Poli(namaPoli: _namaPoliCtrl.text);
+          Navigator.pop(context);
+          Navigator.pushReplacement(context,
+              MaterialPageRoute(builder: (context) => PoliDetail(poli: poli)));
+        },
+        child: const Text("Simpan Perubahan"));
+  }
+}
