86136ff181b0d7264a51a1b8ac33d525324d2c95
alfimonth <alvy1610@gmail.com>
2023-05-21 17:06:06 +0700

    membuat fungsi simpan+detail pasien

--- lib/ui/pasien_form.dart
+++ lib/ui/pasien_form.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:aplikasi_flutter_pertamaku/model/pasien.dart';
+import 'package:aplikasi_flutter_pertamaku/ui/pasien_detail.dart';
 
 class PasienForm extends StatefulWidget {
   const PasienForm({Key? key}) : super(key: key);
@@ -7,6 +9,12 @@ class PasienForm extends StatefulWidget {
 
 class _PasienFormState extends State<PasienForm> {
   final _formKey = GlobalKey<FormState>();
+  final _namaPasienCtrl = TextEditingController();
+  final _noRekamMedisCtrl = TextEditingController();
+  final _tanggalLahirCtrl = TextEditingController();
+  final _nomorTeleponCtrl = TextEditingController();
+  final _alamatCtrl = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,6 +29,7 @@ class _PasienFormState extends State<PasienForm> {
               _fieldTanggalLahir(),
               _fieldNomorTelepon(),
               _fieldAlamat(),
+              const SizedBox(height: 20),
               _tombolSimpan()
             ],
           ),
@@ -28,41 +37,58 @@ class _PasienFormState extends State<PasienForm> {
       ),
     );
   }
-}
 
-_fieldNamaPasien() {
-  return const TextField(
-    decoration: InputDecoration(labelText: "Nama Pasien"),
-  );
-}
+  _fieldNamaPasien() {
+    return TextField(
+      decoration: const InputDecoration(labelText: "Nama Pasien"),
+      controller: _namaPasienCtrl,
+    );
+  }
 
-_fieldNoRekamMedis() {
-  return const TextField(
-    decoration: InputDecoration(labelText: "No Rekam Medis"),
-  );
-}
+  _fieldNoRekamMedis() {
+    return TextField(
+      decoration: const InputDecoration(labelText: "No Rekam Medis"),
+      controller: _noRekamMedisCtrl,
+    );
+  }
 
-_fieldTanggalLahir() {
-  return const TextField(
-    decoration: InputDecoration(labelText: "Tanggal Lahir"),
-  );
-}
+  _fieldTanggalLahir() {
+    return TextField(
+      decoration: const InputDecoration(labelText: "Tanggal Lahir"),
+      controller: _tanggalLahirCtrl,
+    );
+  }
 
-_fieldNomorTelepon() {
-  return const TextField(
-    decoration: InputDecoration(labelText: "Nomor Telepon"),
-  );
-}
+  _fieldNomorTelepon() {
+    return TextField(
+      decoration: const InputDecoration(labelText: "Nomor Telepon"),
+      controller: _nomorTeleponCtrl,
+    );
+  }
 
-_fieldAlamat() {
-  return const TextField(
-    decoration: InputDecoration(labelText: "Alamat"),
-  );
-}
+  _fieldAlamat() {
+    return TextField(
+      decoration: const InputDecoration(labelText: "Alamat"),
+      controller: _alamatCtrl,
+    );
+  }
 
-_tombolSimpan() {
-  return [
-    const SizedBox(height: 20),
-    ElevatedButton(onPressed: () {}, child: const Text("Simpan"))
-  ];
+  _tombolSimpan() {
+    return ElevatedButton(
+        onPressed: () {
+          Pasien pasien = new Pasien({
+            'id': 999,
+            'nama': _namaPasienCtrl.text,
+            'noRm': _noRekamMedisCtrl.text,
+            'tanggalLahir': _tanggalLahirCtrl.text,
+            'nomorTelepon': _tanggalLahirCtrl.text,
+            'alamat': _alamatCtrl.text
+          });
+          Navigator.pushReplacement(
+              context,
+              MaterialPageRoute(
+                  builder: (context) => PasienDetail(pasien: pasien)));
+        },
+        child: const Text("Simpan"));
+  }
 }
