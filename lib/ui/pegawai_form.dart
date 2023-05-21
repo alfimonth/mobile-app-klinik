f6bcab72fc1cc427bb519c3cbc05d7f67d355cf0
alfimonth <alvy1610@gmail.com>
2023-05-21 19:15:01 +0700

    Membuat fungsi simpan+detail pegawai

--- lib/ui/pegawai_form.dart
+++ lib/ui/pegawai_form.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:aplikasi_flutter_pertamaku/model/pegawai.dart';
+import 'package:aplikasi_flutter_pertamaku/ui/pegawai_detail.dart';
 
 class PegawaiForm extends StatefulWidget {
   const PegawaiForm({Key? key}) : super(key: key);
@@ -7,6 +9,13 @@ class PegawaiForm extends StatefulWidget {
 
 class _PegawaiFormState extends State<PegawaiForm> {
   final _formKey = GlobalKey<FormState>();
+  final _namaPegawaiCtrl = TextEditingController();
+  final _nIPCtrl = TextEditingController();
+  final _tanggalLahirCtrl = TextEditingController();
+  final _nomorTeleponCtrl = TextEditingController();
+  final _emailCtrl = TextEditingController();
+  final _passwordCtrl = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -31,36 +40,65 @@ class _PegawaiFormState extends State<PegawaiForm> {
     );
   }
 
-  _fieldNamaPegawai(){
+  _fieldNamaPegawai() {
     return TextField(
-                  decoration: const InputDecoration(labelText: "Nama Pegawai"));
+      decoration: const InputDecoration(labelText: "Nama Pegawai"),
+      controller: _namaPegawaiCtrl,
+    );
   }
 
-  _fieldNIP(){
-    return TextField(decoration: const InputDecoration(labelText: "NIP"));
-  }
-
-  _fieldTangalLahir(){
+  _fieldNIP() {
     return TextField(
-                  decoration:
-                      const InputDecoration(labelText: "Tanggal Lahir"));
+      decoration: const InputDecoration(labelText: "NIP"),
+      controller: _nIPCtrl,
+    );
   }
 
-  _fieldNomorTelepon(){
+  _fieldTangalLahir() {
     return TextField(
-                  decoration:
-                      const InputDecoration(labelText: "Nomor Telepon"));
+      decoration: const InputDecoration(labelText: "Tanggal Lahir"),
+      controller: _tanggalLahirCtrl,
+    );
   }
 
-  _fieldEmail(){
-    return TextField(decoration: const InputDecoration(labelText: "Email"));
-  }
-
-  _fieldPassword(){
+  _fieldNomorTelepon() {
     return TextField(
-                  decoration: const InputDecoration(labelText: "Password"));
+      decoration: const InputDecoration(labelText: "Nomor Telepon"),
+      controller: _nomorTeleponCtrl,
+    );
   }
-  _tombolSimpan(){
-    return ElevatedButton(onPressed: () {}, child: const Text("Simpan"));
+
+  _fieldEmail() {
+    return TextField(
+      decoration: const InputDecoration(labelText: "Email"),
+      controller: _emailCtrl,
+    );
+  }
+
+  _fieldPassword() {
+    return TextField(
+      decoration: const InputDecoration(labelText: "Password"),
+      controller: _passwordCtrl,
+    );
+  }
+
+  _tombolSimpan() {
+    return ElevatedButton(
+        onPressed: () {
+          Pegawai pegawai = new Pegawai({
+            'id': 999,
+            'nip': _nIPCtrl.text,
+            'nama': _namaPegawaiCtrl.text,
+            'tanggalLahir': _tanggalLahirCtrl.text,
+            'nomorTelepon': _nomorTeleponCtrl.text,
+            'email': _emailCtrl.text,
+            'password': _passwordCtrl.text,
+          });
+          Navigator.pushReplacement(
+              context,
+              MaterialPageRoute(
+                  builder: (context) => PegawaiDetail(pegawai: pegawai)));
+        },
+        child: const Text("Simpan"));
   }
 }
